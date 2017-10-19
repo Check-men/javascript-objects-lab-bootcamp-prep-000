49dba62cf86607f5fa5af9c272c2a31c134c1770
Tsuyoshi Akiyama <ka090824@outlook.com>
2017-10-19 07:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = new Object()
 
 function updateObjectWithKeyAndValue(object, key, value) {
   recipes[key] = value
-  var new = object.assign(object, recipes)
-  return new
+  var n = object.assign(object, recipes)
+  return n
 }
